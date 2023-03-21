7075761fa4cc30eb4ede9c092eb6a03715ab878b
Kamrul Hasan <kamrul.h456@gmail.com>
2023-03-21 12:05:51 +0600

    Test Case Wait for DB added

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backend.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST':os.environ.get('DB_HOST'),
         'NAME':os.environ.get('DB_NAME'),
         'USER':os.environ.get('DB_USER'),
