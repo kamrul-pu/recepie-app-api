fe7bf1c9e50828cd14005ba2094447cf41b45834
Kamrul Hasan <kamrul.h456@gmail.com>
2023-03-21 16:44:10 +0600

    Customer user model added

--- app/app/settings.py
+++ app/app/settings.py
@@ -116,3 +116,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
