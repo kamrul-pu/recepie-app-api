c6df4568f6b539fd6b5f9039273511e29e8b1d49
Kamrul Hasan <kamrul.h456@gmail.com>
2023-03-23 12:23:48 +0600

    Error Fixed in Model

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -36,7 +36,7 @@ def create_recipe(user, **params):
 
     defaults.update(params)
 
-    recipe = Recipe(user=user, **defaults)
+    recipe = Recipe.objects.create(user=user, **defaults)
     return recipe
 
 
